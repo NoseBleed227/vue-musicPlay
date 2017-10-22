6ca1c29833c78a0bb8117b57584c8521270b0a7c
lf <lf@qq.com>
2017-10-22 15:57:36 +0800

    代码提交

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+// 导入组件
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
+// 导出app根组件
+export default {
+  // 注册组件
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+
+// 减少移动端点击事件得3秒延迟
+fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
